9402829a9ebf67a22704571b2448189fefc699ad
nurmeden <nurmeden.02@gmail.com>
2022-12-17 20:41:43 +0600

    second commit

---
+++ cmd/main.go
@@ -0,0 +1,13 @@
+package cmd
+
+import (
+	"log"
+	"todo"
+)
+
+func main() {
+	srv := new(todo.Server)
+	if err := srv.Run("8008"); err != nil {
+		log.Fatalf("error occured while running http server: %s", err.Error())
+	}
+}
