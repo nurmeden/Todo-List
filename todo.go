51c117c9bb90092a1282d0abbb0c9f3d39e8f8c0
nurmeden <nurmeden.02@gmail.com>
2022-12-19 22:04:31 +0600

    second

---
+++ todo.go
@@ -0,0 +1,20 @@
+package todo
+
+type TodoList struct {
+	Id          int    `json:"id"`
+	Title       string `json:"title"`
+	Description string `json:"description"`
+}
+
+type UserList struct {
+	Id     int
+	UserId int
+	ListId int
+}
+
+type TodoItem struct {
+	Id          int    `json:"id"`
+	Title       string `json:"title`
+	Description string `json:"description"`
+	Done        bool   `json:"done"`
+}
