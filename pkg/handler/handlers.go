f21b68386ac17bf540ceee714ddc931a3663c438
nurmeden <nurmeden.02@gmail.com>
2022-12-24 19:24:30 +0600

    update handlers

--- pkg/handler/handlers.go
+++ pkg/handler/handlers.go
@@ -3,7 +3,29 @@ package handler
 import (
 	"html/template"
 	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
+
+type Handler struct{}
+
+func (h *Handler) initRoutes() *gin.Engine { // endpoint
+	router := gin.New() // initialize router
+
+	auth := router.Group("/auth")
+	{
+		auth.POST("/sign-up")
+		auth.POST("/sign-in")
+	}
+	api := router.Group("/api")
+	{
+		lists := api.Group("/lists")
+		{
+			lists.POST("/")
+			lists.POST("/")
+		}
+	}
+}
 
 func Create_Page(w http.ResponseWriter, r *http.Request) {
 	tmpl, _ := template.ParseFiles("webpage.html")
