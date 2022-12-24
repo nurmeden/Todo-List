f64ceba836bad9d579b92819c4cca37dd5bfd7a1
nurmeden <nurmeden.02@gmail.com>
2022-12-24 20:04:09 +0600

    updating

---
+++ pkg/handler/handler.go
@@ -0,0 +1,51 @@
+package handler
+
+import (
+	"html/template"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type Handler struct{}
+
+func (h *Handler) InitRoutes() *gin.Engine { // endpoint
+	router := gin.New() // initialize router
+
+	auth := router.Group("/auth")
+	{
+		auth.POST("/sign-up")
+		auth.POST("/sign-in")
+	}
+	api := router.Group("/api")
+	{
+		lists := api.Group("/lists")
+		{
+			lists.POST("/")
+			lists.GET("/")
+			lists.GET("/:id")
+			lists.PUT("/:id")
+			lists.DELETE("/:id")
+
+			items := lists.Group(":id/items")
+			{
+				items.POST("/")
+				items.GET("/")
+				items.GET("/:item_id")
+				items.GET("/:item_id")
+				items.DELETE("/:item_id")
+			}
+		}
+	}
+
+	return router
+}
+
+func Create_Page(w http.ResponseWriter, r *http.Request) {
+	tmpl, _ := template.ParseFiles("webpage.html")
+	err := tmpl.Execute(w, nil)
+	if err != nil {
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+}
